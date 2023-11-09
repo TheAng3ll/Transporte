c3cb204e33f97d848828930ad463c1c2869606be
TheAng3ll <j.a.b.a@outlook.es>
2023-11-09 17:30:18 -0600

    actualizacion

--- resources/views/Plantilla.blade.php
+++ resources/views/Plantilla.blade.php
@@ -7,14 +7,109 @@
 
 </head>
 <body>
-  <p>Holassssssssss</p>
+  <style type="text/css">
+    .form-control
+    {
+      position:relative;
+      top:6px;
+      margin:5px;
+    }
+    #Login
+    {
+      position:relative;
+      margin:5px;
+      top:6px;
+    }
+  </style>
+
+
+
+  {{-- Barra de navegacion --}}
+  <div>
+  <nav style="height: 50px;" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="{{ route("inicio") }}">Transporte</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="">Aplicacion<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        {{-- <a class="nav-link" href="{{ route('admin') }}">Admin</a> --}}
+      </li>
+      <li class="nav-item">
+        {{-- <a class="nav-link disabled" id="test">Disabled</a> --}}
+      </li>
+    </ul>
+    <form class="form-inline mt-2 mt-md-0">
+      
+      {{-- Inicio de secion o registro de cuentas --}}
+      <input type="text" class="form-control" id="user" placeholder="Nombre">
+      <input type="password" class="form-control" id="pass" placeholder="Password">
+      <button class="btn btn-outline-success my-2 my-sm-0" id="Login" type="submit" onclick="login()">Iniciar</button>
+    </form>
+    <button class="btn btn-outline-success my-2 my-sm-0" id="Login" style="top:-2px;" onclick="window.location.href='{{ route("registro") }}'">Registrarse</button>
+  </div>
+</nav>
+</div>
 
 
   {{-- las vistas que heredan de plantilla --}}
   <main role="main">
+    @yield("inicio")
     @yield("registro")
     @yield("principal")
   </main>
+
+
+<footer class="container py-5">
+  <div class="row">
+    <div class="col-12 col-md">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+      <small class="d-block mb-3 text-muted">&copy; 2017-2022</small>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Features</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Cool stuff</a></li>
+        <li><a class="text-muted" href="#">Random feature</a></li>
+        <li><a class="text-muted" href="#">Team feature</a></li>
+        <li><a class="text-muted" href="#">Stuff for developers</a></li>
+        <li><a class="text-muted" href="#">Another one</a></li>
+        <li><a class="text-muted" href="#">Last time</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Resources</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Resource</a></li>
+        <li><a class="text-muted" href="#">Resource name</a></li>
+        <li><a class="text-muted" href="#">Another resource</a></li>
+        <li><a class="text-muted" href="#">Final resource</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Resources</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Business</a></li>
+        <li><a class="text-muted" href="#">Education</a></li>
+        <li><a class="text-muted" href="#">Government</a></li>
+        <li><a class="text-muted" href="#">Gaming</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>About</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Team</a></li>
+        <li><a class="text-muted" href="#">Locations</a></li>
+        <li><a class="text-muted" href="#">Privacy</a></li>
+        <li><a class="text-muted" href="#">Terms</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('Plantilla');
-});
+    return view('inicio/index');
+})->name("inicio");
 
 //vistas
 Route::view("registro","registro/index")->name("registro");
